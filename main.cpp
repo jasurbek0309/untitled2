6f9074621beb1cea893e277e231edc5befee2055
Jasurbek Ruziboyev <j.roziboyev@newuzbekistanuniversity.uz>
2023-03-21 22:36:49 +0500

    power of a

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-    int main(){
-    long long a,b;
-    cin>>a>>b;
-    cout<<a*b<< endl;
+int main(){
+    long long n; cin >> n;
+    vector <int> a;
+
+    for(int i = 1; i <= n; i ++){
+        int x; cin >> x;
+        a.push_back(x);
+    }
+    sort(a.begin(), a.end());
+
+    cout << a[n - 2];
+
     return 0;
-}
+}2
+
+
