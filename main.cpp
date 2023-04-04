0bd585ec205fd79a26a0646072b76e33427d7eef
Jasurbek Ruziboyev <j.roziboyev@newuzbekistanuniversity.uz>
2023-04-05 00:05:11 +0500

    Problem 2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a, b;
-    cin >> a >> b;
-    cout << "The result of sum is equal to " << a + b << endl;
-    cout << "The result of product is equal to " << a * b << endl;
+    float a;
+    cin >> a;
+    cout << "The diameter of circle is equal to " << 2 * a << endl;
+    cout << "The circumference of circle is equal to " << 2 * 3.14 * a << endl;
+    cout << "The area of circle is equal to " << 3.14 * a * a << endl;
     return 0;
 }
